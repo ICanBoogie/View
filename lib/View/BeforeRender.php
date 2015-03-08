4c4a8304b6da922d0f15da42b8040faeea696d0b
Olivier Laviale <olivier.laviale@gmail.com>
2015-03-08 13:19:50 +0100

    Using icanboogie\render v0.4.0

--- lib/View/BeforeRender.php
+++ lib/View/BeforeRender.php
@@ -5,6 +5,13 @@ namespace ICanBoogie\View\View;
 use ICanBoogie\Event;
 use ICanBoogie\View\View;
 
+/**
+ * Event class for the `ICanBoogie\View\View::render:before` event.
+ *
+ * Event hooks may use this event to alter the view before it is rendered.
+ *
+ * @package ICanBoogie\View\View
+ */
 class BeforeRender extends Event
 {
 	public function __construct(View $target)
